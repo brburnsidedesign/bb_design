e47ffee450f355618c25c7fd0eb51f219a7e920c
Will Barbee <will.clayton.barbee@gmail.com>
2016-05-24 12:05:39 -0500

    build

--- app/js/MainCtrl.js
+++ app/js/MainCtrl.js
@@ -1,8 +1,8 @@
 app.controller('MainCtrl', ['$scope', '$state', '$location', '$http', function($scope, $state, $location, $http) {
 }]);
 
-app.run(function($rootScope) {
-	$rootScope.$on('$viewContentLoaded',function($viewContentLoaded){
-    	jQuery('html, body').animate({ scrollTop: 0 }, 300);
-  	});
-});
+// app.run(function($rootScope) {
+// 	$rootScope.$on('$viewContentLoaded',function($viewContentLoaded){
+//     	jQuery('html, body').animate({ scrollTop: 0 }, 300);
+//   	});
+// });
